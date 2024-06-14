66c01ea8655b0ce553b27a8c4725c5d40dfe6f3c
vrapatel2002 <130017460+vrapatel2002@users.noreply.github.com>
2024-06-14 04:19:06 +0000

    IMPLEMENTED CSS

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,8 +5,8 @@ if (!isset($_SESSION['auth'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <head>x
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="icon" href="/favicon.png">
         <title>COSC 4806</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -47,3 +47,4 @@ if (!isset($_SESSION['auth'])) {
     </div>
   </div>
 </nav>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -3,3 +3,4 @@
 require_once 'app/init.php';
 
 $app = new App;
+
